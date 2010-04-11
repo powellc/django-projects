c55fb8d5c6ac9fed84ce5c130f79946eff3691cd
Colin Powell <colin.powell@me.com>
2010-04-11 00:53:07 -0400

    Added an admin file, templates and fixed the urls.

---
+++ sermon/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from sermon.models import Sermon, Reading, Speaker
+
+class SermonAdmin(admin.ModelAdmin):
+    
+    list_display = ('title', 'speaker', 'date', )
+    search_fields = ('title','body','speaker',)
+    list_filter = ('published','speaker',)
+    
+admin.site.register(Sermon, SermonAdmin)
+admin.site.register(Reading)
+admin.site.register(Speaker)
